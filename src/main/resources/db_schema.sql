7ce4f8a832c1cd934bb69f9052bcdf7a35b479a3
M00 <M00@M00A6011-BX310>
2017-08-07 15:59:34 +0800

    sqltest

--- src/main/resources/db_schema.sql
+++ src/main/resources/db_schema.sql
@@ -1,3 +1,9 @@
 /***********************************************
 * 說明：使用sql server時的註解可使用減號「-」
 ***********************************************/
+CREATE schema lifecompany;
+CREATE TABLE `member` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='myTest'
